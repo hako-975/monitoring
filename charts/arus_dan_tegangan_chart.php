f5f71cf4743ed4d53f472930059bd78c3e7999f2
hako-975 <andrifirmansaputra1@gmail.com>
2024-04-28 22:56:37 +0700

    chart

--- charts/arus_dan_tegangan_chart.php
+++ charts/arus_dan_tegangan_chart.php
@@ -11,9 +11,11 @@ $dataUserLogin = dataUserLogin();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Arus dan Tegangan Charts</title>
-  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="https://png.pngtree.com/png-clipart/20200225/original/pngtree-computer-static-graph-monitor-abstract-flat-color-icon-templa-png-image_5254061.jpg">
+  
+  <title>Arus & Tegangan Chart</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -21,6 +23,7 @@ $dataUserLogin = dataUserLogin();
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -31,7 +34,6 @@ $dataUserLogin = dataUserLogin();
   <!-- Main Sidebar Container -->
   <?php include_once '../include/sidebar.php'; ?>
 
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -39,7 +41,7 @@ $dataUserLogin = dataUserLogin();
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Arus & Tegangan Charts</h1>
+            <h1>Arus & Tegangan Chart</h1>
           </div>
         </div>
       </div><!-- /.container-fluid -->
@@ -49,74 +51,47 @@ $dataUserLogin = dataUserLogin();
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-12">
-            <!-- interactive chart -->
-            <div class="card card-primary card-outline">
+          <div class="col-md-12">
+            <!-- AREA CHART -->
+            <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">
-                  <i class="far fa-chart-bar"></i>
-                  Arus
-                </h3>
-
-                <div class="card-tools">
-                  Real time
-                  <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                    <button type="button" class="btn btn-default btn-sm active" data-toggle="on">On</button>
-                    <button type="button" class="btn btn-default btn-sm" data-toggle="off">Off</button>
-                  </div>
-                </div>
+                <h3 class="card-title">Arus</h3>
               </div>
               <div class="card-body">
-                <div id="interactive-arus" style="height: 300px;"></div>
+                <div class="chart">
+                  <canvas id="arusChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
               </div>
-              <!-- /.card-body-->
+              <!-- /.card-body -->
             </div>
             <!-- /.card -->
-
           </div>
-          <!-- /.col -->
+          <!-- /.col (LEFT) -->
+          <div class="col-md-12">
+            <!-- LINE CHART -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Tegangan</h3>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="teganganChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col (RIGHT) -->
         </div>
         <!-- /.row -->
-      </div>
-    </section>
-    
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <!-- interactive chart -->
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h3 class="card-title">
-                  <i class="far fa-chart-bar"></i>
-                  Tegangan
-                </h3>
-
-                <div class="card-tools">
-                  Real time
-                  <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                    <button type="button" class="btn btn-default btn-sm active" data-toggle="on">On</button>
-                    <button type="button" class="btn btn-default btn-sm" data-toggle="off">Off</button>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <div id="interactive-tegangan" style="height: 300px;"></div>
-              </div>
-              <!-- /.card-body-->
-            </div>
-            <!-- /.card -->
-
-          </div>
-          <!-- /.col -->
-        </div>
       </div><!-- /.container-fluid -->
     </section>
-        <!-- /.content -->
+    <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
   <?php include_once '../include/footer.php'; ?>
+  <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
@@ -124,170 +99,136 @@ $dataUserLogin = dataUserLogin();
 <script src="../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../plugins/chart.js/Chart.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../dist/js/adminlte.min.js"></script>
-<!-- FLOT CHARTS -->
-<script src="../plugins/flot/jquery.flot.js"></script>
-<!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
-<script src="../plugins/flot/plugins/jquery.flot.resize.js"></script>
-<!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-<script src="../plugins/flot/plugins/jquery.flot.pie.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-<!-- Page specific script -->
 <script>
 $(function () {
-  var data        = [],
-      totalPoints = 100
+  // Get context with jQuery - using jQuery's .get() method.
+  var arusChartCanvas = $('#arusChart').get(0).getContext('2d')
 
-  function getRandomData() {
-
-    if (data.length > 0) {
-      data = data.slice(1)
-    }
-
-    // Do a random walk
-    while (data.length < totalPoints) {
-
-      var prev = data.length > 0 ? data[data.length - 1] : 50,
-          y    = prev + Math.random() * 10 - 5
-
-      if (y < 0) {
-        y = 0
-      } else if (y > 100) {
-        y = 100
-      }
-
-      data.push(y)
-    }
-
-    // Zip the generated y values with the x values
-    var res = []
-    for (var i = 0; i < data.length; ++i) {
-      res.push([i, data[i]])
-    }
-
-    return res
-  }
-
-  var interactive_arus = $.plot('#interactive-arus', [
+  var arusChartData = {
+    labels  : [],
+    datasets: [
       {
-        data: getRandomData(),
+        label               : 'Arus',
+        borderColor         : 'rgb(0,123,255)',
+        pointRadius         : 5,
+        pointHoverRadius    : 8,
+        pointColor          : 'rgb(0,123,255)',
+        pointStrokeColor    : 'rgb(0,123,255)',
+        pointHighlightFill  : 'rgb(0,123,255)',
+        pointHighlightStroke: 'rgb(0,123,255)',
+        data                : []
       }
-    ],
-    {
-      grid: {
-        borderColor: '#f3f3f3',
-        borderWidth: 1,
-        tickColor: '#f3f3f3'
-      },
-      series: {
-        color: '#3c8dbc',
-        lines: {
-          lineWidth: 2,
-          show: true,
-          fill: true,
-        },
-      },
-      yaxis: {
-        min: 0,
-        max: 100,
-        show: true
-      },
-      xaxis: {
-        show: true
-      }
-    }
-  )
+    ]
+  }
 
-  var interactive_tegangan = $.plot('#interactive-tegangan', [
-      {
-        data: getRandomData(),
-      }
-    ],
-    {
-      grid: {
-        borderColor: '#f3f3f3',
-        borderWidth: 1,
-        tickColor: '#f3f3f3'
-      },
-      series: {
-        color: '#3c8dbc',
-        lines: {
-          lineWidth: 2,
-          show: true,
-          fill: true,
-        },
-      },
-      yaxis: {
-        min: 0,
-        max: 100,
-        show: true
-      },
-      xaxis: {
-        show: true
-      }
-    }
-  )
-
-  var updateInterval = 500 //Fetch data ever x milliseconds
-  var realtime       = 'on' //If == to on then fetch data every x seconds. else stop fetching
-  function updateArus() {
-
-    interactive_arus.setData([getRandomData()])
-
-    // Since the axes don't change, we don't need to call plot.setupGrid()
-    interactive_arus.draw()
-    if (realtime === 'on') {
-      setTimeout(updateArus, updateInterval)
+  var arusChartOptions = {
+    maintainAspectRatio : false,
+    responsive : true,
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines : {
+          display : true,
+        }
+      }],
+      yAxes: [{
+        gridLines : {
+          display : true,
+        }
+      }]
     }
   }
 
-  //INITIALIZE REALTIME DATA FETCHING
-  if (realtime === 'on') {
-    updateArus()
-  }
-  //REALTIME TOGGLE
-  $('#realtime .btn').click(function () {
-    if ($(this).data('toggle') === 'on') {
-      realtime = 'on'
-    }
-    else {
-      realtime = 'off'
-    }
-    updateArus()
+  // This will get the first returned node in the jQuery collection.
+  var arus = new Chart(arusChartCanvas, {
+    type: 'line',
+    data: arusChartData,
+    options: arusChartOptions
   })
 
-  function updateTegangan() {
+  // Get context with jQuery - using jQuery's .get() method.
+  var teganganChartCanvas = $('#teganganChart').get(0).getContext('2d')
 
-    interactive_tegangan.setData([getRandomData()])
+  var teganganChartData = {
+    labels  : [],
+    datasets: [
+      {
+        label               : 'Tegangan',
+        borderColor         : 'rgb(23,162,184)',
+        pointRadius         : 5,
+        pointHoverRadius    : 8,
+        pointColor          : 'rgb(23,162,184)',
+        pointStrokeColor    : 'rgb(23,162,184)',
+        pointHighlightFill  : 'rgb(23,162,184)',
+        pointHighlightStroke: 'rgb(23,162,184)',
+        data                : []
+      }
+    ]
+  }
 
-    // Since the axes don't change, we don't need to call plot.setupGrid()
-    interactive_tegangan.draw()
-    if (realtime === 'on') {
-      setTimeout(updateTegangan, updateInterval)
+  var teganganChartOptions = {
+    maintainAspectRatio : false,
+    responsive : true,
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines : {
+          display : true,
+        }
+      }],
+      yAxes: [{
+        gridLines : {
+          display : true,
+        }
+      }]
     }
   }
 
-  //INITIALIZE REALTIME DATA FETCHING
-  if (realtime === 'on') {
-    updateTegangan()
-  }
-  //REALTIME TOGGLE
-  $('#realtime .btn').click(function () {
-    if ($(this).data('toggle') === 'on') {
-      realtime = 'on'
-    }
-    else {
-      realtime = 'off'
-    }
-    updateTegangan()
+  // This will get the first returned node in the jQuery collection.
+  var tegangan = new Chart(teganganChartCanvas, {
+    type: 'line',
+    data: teganganChartData,
+    options: teganganChartOptions
   })
-  /*
-   * END INTERACTIVE CHART
-   */
+
+  // Function to fetch data from PHP script
+  function fetchDataArrayArusTegangan() {
+    $.ajax({
+      url: '../fetch_data_array_arus_tegangan.php',
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        arusChartData.labels = data.label_arus_tegangan_array;
+        arusChartData.datasets[0].data = data.arus_array;
+        arus.update();
+        teganganChartData.labels = data.label_arus_tegangan_array;
+        teganganChartData.datasets[0].data = data.tegangan_array;
+        tegangan.update();
+      },
+      error: function(xhr, status, error) {
+        console.error('Error fetching data:', error);
+      }
+    });
+  }
+
+  // Fetch data initially
+  fetchDataArrayArusTegangan();
+
+  // Fetch data every 5 seconds
+  setInterval(fetchDataArrayArusTegangan, 5000);
+
+// ----------------------------------------------------
+
 })
-
 </script>
 </body>
 </html>
